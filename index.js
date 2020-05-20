c5718736a9202cd3aa10bc603ac07e290d91adac
Lemon-Rain <2242195359@qq.com>
2020-05-20 20:36:27 +0800

    修复轮播图bug

--- index.js
+++ index.js
@@ -44,37 +44,31 @@ var rotatePic = 0;
 
 function Rotate() {
     var promo_list = document.querySelector('.promo-list');
-    promo_list.style.marginLeft = rotatePic + 'px';
-    rotatePic -= 604;
-    rotatePic %= 3020;
+    var span_list = document.querySelector('.promoTrigger').querySelectorAll('span');
+    for (var i = 0; i < span_list.length; i++) {
+        if (rotatePic == i) {
+            span_list[i].id = 'promo-active';
+        } else {
+            span_list[i].id = '';
+        }
+    }
+    promo_list.style.marginLeft = rotatePic * -604 + 'px';
+    rotatePic++;
+    rotatePic %= 5;
 }
 
 function setPromoPic(n) {
+    stopRotate();
     rotatePic = n;
     Rotate();
-    stopRotateTimer();
 }
 
-function startRotateTimer() {
-    rotateTimer = setInterval("autoRotate()", 5000);
+function startRotate() {
+    rotateTimer = setInterval("Rotate()", 5000);
 }
 
-function stopRotateTimer() {
+function stopRotate() {
     clearTimeout(rotateTimer);
 }
 
-startRotateTimer();
-
-function setNewsList(n) {
-    var newsBox = document.querySelector('.news-slide-box');
-    newsBox.style.marginLeft = n * -359 + 'px'
-    var newsTab = document.querySelector('.news_tab');
-    var tabList = newsTab.querySelectorAll('a');
-    for (var i = 0; i < tabList.length; i++){
-        if (i == n) {
-            tabList[i].id = 'news_tab_on';
-        } else {
-            tabList[i].id = '';
-        }
-    }
-}
+startRotate();
